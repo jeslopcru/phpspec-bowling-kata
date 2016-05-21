baeb64717f6f0f338ec8844adfcc1b603ec1fc1c
jeslopcru <jeslopcru@gmail.com>
2016-05-21 22:32:19 +0200

    JesúsLC PR: more readable public methods - checkXX for methods that throw exceptions - IsCorrectValue explained with constants, maybe a little verbose

--- src/CodeWrong/Roll.php
+++ src/CodeWrong/Roll.php
@@ -4,15 +4,26 @@ namespace CodeWrong;
 
 class Roll
 {
-	protected $pins;
+    const MAX_NUMBER_OF_PINS = 10;
+    const ZERO = 0;
+    protected $pins;
 
     public function __construct($pins)
     {
-    	if ($pins < 0 or $pins > 10) {
-    		throw new \Exception("Incorrect value.");
-    	}
-
+        $this->checkPins($pins);
         $this->pins = $pins;
+    }
+
+    private function checkPins($pins)
+    {
+        if ($this->isCorrectValue($pins)) {
+            throw new \Exception("Incorrect value.");
+        }
+    }
+
+    private function isCorrectValue($pins)
+    {
+        return $pins < self::ZERO or $pins > self::MAX_NUMBER_OF_PINS;
     }
 
     public function score()
